3b1bed52e85fccc3fd24de7963d40c9c3fa97ae3
juli0mendes <jmendes@live.com>
2019-07-24 17:28:43 -0300

    Streams: Collectors (toList, groupingBy, joining)

--- src/br/com/juli0mendes/Java8Stream.java
+++ src/br/com/juli0mendes/Java8Stream.java
@@ -1,7 +1,10 @@
 package br.com.juli0mendes;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * @author Julio Cesar Mendes
@@ -61,5 +64,47 @@ public class Java8Stream {
 		listaInteiros.stream().limit(3).map(e -> e * 2).forEach(e -> System.out.println(e));
 	
 		System.out.println("\n");
+		
+		System.out.println(listaInteiros.stream().map(e -> e * 2).count());
+		
+		System.out.println("\n");
+		
+		System.out.println(listaInteiros.stream().filter(e -> e % 2 == 0).count());
+		
+		System.out.println("\n");
+		
+		System.out.println(listaInteiros.stream().filter(e -> e % 2 == 0).min(Comparator.naturalOrder()).get());
+		
+		System.out.println("\n");
+		
+		System.out.println(listaInteiros.stream().filter(e -> e % 2 == 0).max(Comparator.naturalOrder()).get());
+		
+		System.out.println("\n");
+		
+		List<Integer> novaListaInteiros = listaInteiros.stream().filter(e -> e % 2 == 0).map(e -> e * 3).collect(Collectors.toList());
+		System.out.println(novaListaInteiros);
+		
+		System.out.println("\n");
+		
+		Map<Boolean, List<Integer>> mapa1 = listaInteiros.stream().map(e -> e *3).collect(Collectors.groupingBy(e -> e % 2 == 0));
+		System.out.println(mapa1);
+		
+		System.out.println("\n");
+		
+		Map<Integer, List<Integer>> mapa2 = listaInteiros.stream().collect(Collectors.groupingBy(e -> e % 3));
+		System.out.println(mapa2);
+		
+		System.out.println("\n");
+		
+		String collect = listaInteiros.stream().map(e -> String.valueOf(e)).collect(Collectors.joining());
+		System.out.println(collect);
+		
+		System.out.println("\n");
+		
+		System.out.println(listaInteiros.stream().map(e -> String.valueOf(e)).collect(Collectors.joining(";")));
+		
+		System.out.println("\n");
+		
+		
 	}
 }
