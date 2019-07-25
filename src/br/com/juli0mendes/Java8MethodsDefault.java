e8eaf755f9765be23c3c077589f93ad354e02a2d
juli0mendes <jmendes@live.com>
2019-07-25 16:38:19 -0300

    Methods default.

--- src/br/com/juli0mendes/Java8MethodsDefault.java
+++ src/br/com/juli0mendes/Java8MethodsDefault.java
@@ -3,7 +3,44 @@ package br.com.juli0mendes;
 public class Java8MethodsDefault {
 
 	public static void main(String[] args) {
-
+		new Pessoa().correr();
+		new Carro().correr();
+		new Avestruz().correr();
+		new Pessoa().correrRapido();;
+		new Carro().correrRapido();;
+		new Avestruz().correrRapido();;
 	}
-
+	
+	interface Corredor {
+		abstract void correr();
+		default void correrRapido() {
+			System.out.println("Correr rápido.");
+		}
+	}
+	
+	static class Pessoa implements Corredor {
+		@Override
+		public void correr() {
+			System.out.println("Pessoa correndo.");
+		}
+		
+		@Override
+		public void correrRapido() {
+			System.out.println("Pessoa correndo rápido.");
+		}
+	}
+	
+	static class Carro implements Corredor {
+		@Override
+		public void correr() {
+			System.out.println("Carro correndo.");
+		}
+	}
+	
+	static class Avestruz implements Corredor {
+		@Override
+		public void correr() {
+			System.out.println("Avestruz correndo.");
+		}
+	}
 }
